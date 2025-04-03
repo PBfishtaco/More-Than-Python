2b19e5599de64a35605ad1d50cf8499bd7e019f6
PBfishtaco <peanutbutterfishtaco@gmail.com>
2025-04-03 19:32:34 -0400

    Create ceaser cypher.py

---
+++ ceaser cypher.py
@@ -0,0 +1,7 @@
+bvhFDFDs = int(input("Number: "))
+xavcDSfv = input("Text: ")
+xavcDSfv = list(xavcDSfv)
+for i in range(len(xavcDSfv)):
+    xavcDSfv[i] = chr(ord(xavcDSfv[i]) + bvhFDFDs)
+print("")
+print("".join(xavcDSfv))
