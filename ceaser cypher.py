4e36f2ed4e54868150ad286a55e3e95a14f4cbed
PBfishtaco <peanutbutterfishtaco@gmail.com>
2025-04-03 19:33:47 -0400

    Update ceaser cypher.py

--- ceaser cypher.py
+++ ceaser cypher.py
@@ -1,7 +1,8 @@
-bvhFDFDs = int(input("Number: "))
+bvhFDFDs = int(input("fart: "))
 xavcDSfv = input("Text: ")
 xavcDSfv = list(xavcDSfv)
 for i in range(len(xavcDSfv)):
     xavcDSfv[i] = chr(ord(xavcDSfv[i]) + bvhFDFDs)
 print("")
 print("".join(xavcDSfv))
+input()
