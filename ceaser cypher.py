2357d9cbb9c167c07dee608582c3a772d2e5b2b5
PBfishtaco <peanutbutterfishtaco@gmail.com>
2025-04-03 20:06:41 -0400

    Finished ceaser cypher
    added error handling in a infinite loop, breakable by inputting nothing aka pressing eneter

--- ceaser cypher.py
+++ ceaser cypher.py
@@ -1,8 +1,23 @@
-bvhFDFDs = int(input("Number: "))s
-xavcDSfv = input("Text: ")
-xavcDSfv = list(xavcDSfv)
-for i in range(len(xavcDSfv)):
-    xavcDSfv[i] = chr(ord(xavcDSfv[i]) + bvhFDFDs)
-print("")
-print("".join(xavcDSfv))
-input()
+kill = False
+while True:
+    while True:
+        if kill == True:
+            exit()
+        try:
+            number = input("Number: ")
+            if number == "":
+                kill = True
+            number = int(number)
+            letters = input("Text: ")
+        except:
+            print("err\n")
+        else:
+            break
+        exit()
+    else:
+        print("word:", letters, sep="")
+    letters = list(letters)
+    for i in range(len(letters)):
+        letters[i] = chr(ord(letters[i]) + number)
+    print("".join(letters))
+    print("")
